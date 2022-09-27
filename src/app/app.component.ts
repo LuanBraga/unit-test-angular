3105abfd0e3a5eefcf5b2d46fc0f97ec381f3767
LuanBraga <luanbraga.lb@gmail.com>
2022-09-27 13:36:58 -0300

    implements getTodos()

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Todo } from './shared/models/todo.model';
+import { TodoService } from './shared/services/todo.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'unit-test-angular';
+
+  subscriptions$: Subscription[] = [];
+  todos: Todo[];
+
+  constructor(private todoService: TodoService) { }
+
+  subscriptions: Subscription[];
+
+  ngOnInit(): void {
+    this.subscriptions$.push(
+      this.todoService.getTodos().subscribe({
+        next: todos => this.todos = todos,
+        error: err => { },
+        complete: () => console.log(this.todos)
+      })
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions$.forEach(sub => sub.unsubscribe());
+  }
+  
 }
--- src/app/shared/services/todo.service.ts
+++ src/app/shared/services/todo.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Todo } from '../models/todo.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodoService {
 
-  constructor() { }
+  readonly API = 'https://jsonplaceholder.typicode.com/todos/'
+
+  constructor(private http: HttpClient) { }
+
+  getTodos() {
+    return this.http.get<Todo[]>(`${this.API}`);
+  }
 }
