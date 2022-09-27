bc44fc7126c527dde642cef1a4bc7474d861bdb2
LuanBraga <luanbraga.lb@gmail.com>
2022-09-27 15:12:53 -0300

    implements service test

--- src/app/shared/services/todo.service.spec.ts
+++ src/app/shared/services/todo.service.spec.ts
@@ -1,12 +1,52 @@
 import { TestBed } from '@angular/core/testing';
 
-import { TodoService } from './todo.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing'
 
-describe('TodoService', () => {
+import { TodoService } from './todo.service';
+import { of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+
+fdescribe('TodoService', () => {
   let service: TodoService;
 
+  //With SpyObject
+  // let httpClientSpy: jasmine.SpyObj<HttpClient>
+
+  // beforeEach(() => {
+  //   httpClientSpy = jasmine.createSpyObj('HttpClient', ['get']);
+
+  //   service = new TodoService(httpClientSpy);
+  // });
+
+  //With TestBed using Stub
+  // const httpStub = {
+  //   get: () => of([
+  //     {
+  //       userId: 1,
+  //       id: 1, title: 'delectus aut autem',
+  //       completed: false
+  //     }
+  //   ])
+  // }
+
+  // beforeEach(() => {
+  //   TestBed.configureTestingModule({
+  //     providers: [
+  //       {
+  //         provide: HttpClient,
+  //         useValue: httpStub
+  //       }
+  //     ]
+  //   });
+  //   service = TestBed.inject(TodoService);
+  // });
+
+  //with TestBed using TestModule
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [ HttpClientTestingModule ] 
+    });
+    
     service = TestBed.inject(TodoService);
   });
 
